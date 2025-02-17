21cc26baa42e47671507c98f9e2d45ebeb1662c3
gonrogon <gonrogon@gmail.com>
2025-02-17 17:15:11 +0100

    Add toggle read status functionality for books
    Implements read status toggling functionality with:
    - Event delegation for dynamic UI elements.
    - DOM updates to reflect the new state instantly
    - UI synchronization with library array data

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+// SVG for the button to delete a book (https://pictogrammers.com/library/mdi/icon/trash-can/)
+const svgDelete = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20">
+    <title>delete</title>
+    <path d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M9,8H11V17H9V8M13,8H15V17H13V8Z" />
+</svg>`;
+// SVG for the button to mark a book as read (https://pictogrammers.com/library/mdi/icon/eye-check/)
+const svgRead = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20">
+    <title>read</title>
+    <path d="M23.5,17L18.5,22L15,18.5L16.5,17L18.5,19L22,15.5L23.5,17M12,9A3,3 0 0,1 15,12A3,3 0 0,1 12,15A3,3 0 0,1 9,12A3,3 0 0,1 12,9M12,17C12.5,17 12.97,16.93 13.42,16.79C13.15,17.5 13,18.22 13,19V19.45L12,19.5C7,19.5 2.73,16.39 1,12C2.73,7.61 7,4.5 12,4.5C17,4.5 21.27,7.61 23,12C22.75,12.64 22.44,13.26 22.08,13.85C21.18,13.31 20.12,13 19,13C18.22,13 17.5,13.15 16.79,13.42C16.93,12.97 17,12.5 17,12A5,5 0 0,0 12,7A5,5 0 0,0 7,12A5,5 0 0,0 12,17Z" />
+</svg>`;
+// SVG for the button to mark a book as not read (https://pictogrammers.com/library/mdi/icon/eye-remove/)
+const svgNotRead = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20">
+    <title>not read</title>
+    <path d="M22.54 16.88L20.41 19L22.54 21.12L21.12 22.54L19 20.41L16.88 22.54L15.47 21.12L17.59 19L15.47 16.88L16.88 15.47L19 17.59L21.12 15.47L22.54 16.88M12 9C10.34 9 9 10.34 9 12S10.34 15 12 15 15 13.66 15 12 13.66 9 12 9M12 17C9.24 17 7 14.76 7 12S9.24 7 12 7 17 9.24 17 12C17 12.5 16.9 13 16.77 13.43C17.46 13.16 18.21 13 19 13C20.12 13 21.17 13.32 22.07 13.85C22.43 13.27 22.74 12.65 23 12C21.27 7.61 17 4.5 12 4.5S2.73 7.61 1 12C2.73 16.39 7 19.5 12 19.5C12.35 19.5 12.69 19.5 13.03 19.45C13 19.3 13 19.15 13 19C13 18.21 13.16 17.46 13.43 16.77C13 16.9 12.5 17 12 17Z" />
+</svg>`;
+// Array with the books.
 const library = [];
 
 function Book(title, author, pages, read) {
@@ -32,14 +48,15 @@ function addBookToPage(book, index = -1) {
 
     const elemControls = document.createElement("div");
     elemControls.classList.add("controls");
-    elemControls.innerHTML =
-        `<button class="delete" ` + (index >= 0 ? ` data-index="${index}"` : "") + `>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20">
-                <title>delete</title>
-                <path d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M9,8H11V17H9V8M13,8H15V17H13V8Z" />
-            </svg>
-        </button>`;
-  
+    elemControls.innerHTML = `
+        <button title="Remove book" class="delete"${index >= 0 ? ` data-index="${index}"` : ""}>
+            ${svgDelete}
+        </button>
+        <button title="${book.read ? "Mark as unread" : "Mark as read"}" class="read-status"${index >= 0 ? ` data-index="${index}"` : ""}>
+            ${book.read ? svgNotRead : svgRead}
+        </button>
+    `;
+
     const elemTitle = document.createElement("h2");
     elemTitle.innerText = book.title;
 
@@ -124,7 +141,6 @@ function setupBookDelete() {
     const elem = document.querySelector("#library");
     elem.addEventListener("click", event => {
         const button = event.target.closest("button.delete");
-        // Check if the target is a delete button.
         if (!button) {
             return;
         }
@@ -139,6 +155,20 @@ function setupBookDelete() {
     });
 }
 
+function setupBookReadStatus() {
+    const elem = document.querySelector("#library");
+    elem.addEventListener("click", event => {
+        const button = event.target.closest("button.read-status");
+        if (!button) {
+            return;
+        }
+
+        const index = button.dataset.index;
+        library[index].toggleRead();
+        syncPage();
+    });
+}
+
 addBookToLibrary("The Hobbit", "J. R. R. Tolkien", 121, false);
 addBookToLibrary("The Lord of the Ring", "J. R. R. Tolkien", 410, false);
 addBookToLibrary("Gateway", "Frederik Pohl", 234, true);
@@ -147,3 +177,4 @@ addBookToLibrary("Ringworld", "Larry Niven", 212, true);
 syncPage();
 setupAddBookForm();
 setupBookDelete();
+setupBookReadStatus();
